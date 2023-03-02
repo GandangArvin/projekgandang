5fee73607d056b43fdb3d471a1d749cd0132260f
GandangArvin <gandangarvinhakim@smkwikrama.sch.id>
2023-03-02 08:13:28 +0700

    kenzibau

--- update.php
+++ update.php
@@ -39,11 +39,7 @@ if (isset($_POST["submit"])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Input Data Siswa</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
-    <style>
-        body {
-            background-color: #0F6292;
-        }
-    </style>
+    <link rel="stylesheet" href="update.css">
 
 </head>
 
